ee7c430b67469a25275b5347ceb0eac079f771d5
andre <andre@localhost>
2014-09-09 23:38:40 -0300

    configurando o projeto

---
+++ src/main/java/br/ufg/calendario/components/Calendario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ufg.calendario.components;
+
+import java.io.Serializable;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author andre
+ */
+
+@Component
+public class Calendario implements Serializable {
+    
+    public String getCalendar() {
+        return "meu calendário";
+    }
+    
+}
