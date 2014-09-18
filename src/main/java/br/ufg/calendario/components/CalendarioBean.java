ce0826e1b63ac307c7df6b551870a9a1202a52ce
andrexterz <andrexterz@gmail.com>
2014-09-18 20:00:00 -0300

    adicionadas classes de modelo

--- src/main/java/br/ufg/calendario/components/CalendarioBean.java
+++ src/main/java/br/ufg/calendario/components/CalendarioBean.java
@@ -5,9 +5,14 @@
  */
 package br.ufg.calendario.components;
 
+import br.ufg.calendario.models.Calendario;
+import br.ufg.calendario.models.Evento;
+import br.ufg.calendario.models.Interessado;
+import br.ufg.calendario.models.Regional;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import org.primefaces.model.LazyDataModel;
@@ -23,20 +28,89 @@ import org.springframework.stereotype.Component;
 @Scope(value = "request")
 public class CalendarioBean implements Serializable {
 
-    private final LazyDataModel eventos;
+    private final LazyDataModel<Evento> eventos;
 
     public CalendarioBean() {
-        eventos = new LazyDataModel() {
+        final List<Evento> datasource = new ArrayList<>();
+        for (int i = 0; i <= 100; i++) {
+            Evento evt = new Evento(
+                    "Evento: " + i,
+                    Calendar.getInstance().getTime(),
+                    Calendar.getInstance().getTime(),
+                    "Descrição " + i, new Calendario(),
+                    new ArrayList<Regional>(),
+                    new ArrayList<Interessado>());
+            datasource.add(evt);
+        }
+
+        eventos = new LazyDataModel<Evento>() {
+            @Override
+            public Evento getRowData(String rowKey) {
+                //modifievt este metodo quando o banco estiver funcionando
+                for (Object evt : datasource) {
+                    if (((Evento) evt).getId().equals(Long.parseLong(rowKey))) {
+                        return (Evento) evt;
+                    }
+                }
+                return null;
+            }
 
             @Override
-            public List load(int first, int pageSize, String sortField, SortOrder sortOrder, Map filters) {
-                return super.load(first, pageSize, sortField, sortOrder, filters); //To change body of generated methods, choose Tools | Templates.
+            public Object getRowKey(Evento evento) {
+                return evento.getId();
+            }
+
+            @Override
+            public List<Evento> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                List<Evento> data = new ArrayList<>();
+                //filter
+                for (Object evt : datasource) {
+                    boolean match = true;
+
+                    if (filters != null) {
+                        for (Iterator<String> it = filters.keySet().iterator(); it.hasNext();) {
+                            try {
+                                String filterProperty = it.next();
+                                Object filterValue = filters.get(filterProperty);
+                                String fieldValue = String.valueOf(evt.getClass().getField(filterProperty).get(evt));
+
+                                if (filterValue == null || fieldValue.startsWith(filterValue.toString())) {
+                                    match = true;
+                                } else {
+                                    match = false;
+                                    break;
+                                }
+                            } catch (Exception e) {
+                                match = false;
+                            }
+                        }
+                    }
+
+                    if (match) {
+                        data.add((Evento) evt);
+                    }
+                }
+                //rowCount
+                int dataSize = data.size();
+                this.setRowCount(dataSize);
+
+                //paginate
+                if (dataSize > pageSize) {
+                    try {
+                        System.out.println("paginating...");
+                        return data.subList(first, first + pageSize);
+                    } catch (IndexOutOfBoundsException e) {
+                        return data.subList(first, first + (dataSize % pageSize));
+                    }
+                } else {
+                    return data;
+                }
             }
         };
 
     }
 
-    public LazyDataModel getEventosRecentes() {
+    public LazyDataModel<Evento> getEventosRecentes() {
         return eventos;
     }
 }
--- src/main/java/br/ufg/calendario/models/Evento.java
+++ src/main/java/br/ufg/calendario/models/Evento.java
@@ -22,6 +22,20 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 public class Evento extends Base {
+
+    public Evento() {
+    }
+
+    public Evento(String titulo, Date inicio, Date termino, String descricao, Calendario calendario, List<Regional> regional, List<Interessado> interessado) {
+        this.titulo = titulo;
+        this.inicio = inicio;
+        this.termino = termino;
+        this.descricao = descricao;
+        this.calendario = calendario;
+        this.regional = regional;
+        this.interessado = interessado;
+    }
+    
     
     @NotNull   
     @Column
@@ -48,4 +62,102 @@ public class Evento extends Base {
     
     @OneToMany
     private List<Interessado> interessado;
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the inicio
+     */
+    public Date getInicio() {
+        return inicio;
+    }
+
+    /**
+     * @param inicio the inicio to set
+     */
+    public void setInicio(Date inicio) {
+        this.inicio = inicio;
+    }
+
+    /**
+     * @return the termino
+     */
+    public Date getTermino() {
+        return termino;
+    }
+
+    /**
+     * @param termino the termino to set
+     */
+    public void setTermino(Date termino) {
+        this.termino = termino;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the calendario
+     */
+    public Calendario getCalendario() {
+        return calendario;
+    }
+
+    /**
+     * @param calendario the calendario to set
+     */
+    public void setCalendario(Calendario calendario) {
+        this.calendario = calendario;
+    }
+
+    /**
+     * @return the regional
+     */
+    public List<Regional> getRegional() {
+        return regional;
+    }
+
+    /**
+     * @param regional the regional to set
+     */
+    public void setRegional(List<Regional> regional) {
+        this.regional = regional;
+    }
+
+    /**
+     * @return the interessado
+     */
+    public List<Interessado> getInteressado() {
+        return interessado;
+    }
+
+    /**
+     * @param interessado the interessado to set
+     */
+    public void setInteressado(List<Interessado> interessado) {
+        this.interessado = interessado;
+    }
 }
